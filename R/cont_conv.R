f7906aa207620828b9fb9452c2884296e7ab2a2e
tnagler <thomas.nagler@tum.de>
2017-02-08 21:03:00 +0100

    refactor cont_conv

--- R/cont_conv.R
+++ R/cont_conv.R
@@ -1,28 +1,27 @@
 #' Continuous convolution
 #'
-#' Applies the continuous convolution trick, i.e. adding continuous noise to
-#' all discrete variables. If a variable should be treated as discrete, declare
-#' it as [ordered()].
+#' Applies the continuous convolution trick, i.e. adding continuous noise to all
+#' discrete variables. If a variable should be treated as discrete, declare it
+#' as [ordered()]. Factors are expanded into binary dummy codes.
 #'
 #' @param x data; numeric matrix or data frame.
-#' @param b scale parameter of the UPSB distribution (see, [dupsb()]).
-#' @param ell smoothness parameter of the UPSB distribution (see, [dupsb()]).
-#' The estimator uses the Epanechnikov kernel for smoothing and the UPSB for
-#' continuous convolution (default parameters correspond to the
-#' \eqn{U[-0.5, 0.5]} distribution).
-#' @param ... unused.
+#' @param theta scale parameter of the USB distribution (see, [dusb()]).
+#' @param nu smoothness parameter of the USB distribution (see, [dusb()]). The
+#'   estimator uses the Epanechnikov kernel for smoothing and the USB for
+#'   continuous convolution (default parameters correspond to the \eqn{U[-0.5,
+#'   0.5]} distribution).
+#' @param eval logical; if `TRUE`, no noise is added (useful for evaluating an
+#'   estimator; adding noise is only useful for estimation. The output is a
+#'   matrix rather
 #'
 #' @return A data frame with noise added to each discrete variable (ordered
-#' columns).
+#'   columns).
 #'
-#' The UPSB distribution ([dupsb()]) is used as the noise distribution. Discrete
-#' variables are assumed to be integer-valued.
+#' @details The UPSB distribution ([dusb()]) is used as the noise distribution.
+#' Discrete variables are assumed to be integer-valued.
 #'
-#' @references
-#' Nagler, T. (2017).
-#' Nonparametric estimation of probability densities when some variables are
-#' discrete.
-#' Unpublished manuscript.
+#' @references Nagler, T. (2017). Nonparametric estimation of probability
+#'   densities when some variables are discrete. Unpublished manuscript.
 #'
 #' @examples
 #' Z <- rbinom(100, 6, 0.3)  # discrete variable
@@ -34,25 +33,87 @@
 #' points(cont_conv(dat), col = 2)
 #'
 #' @export
-cont_conv <- function(x, b = 0, ell = 5, ....) {
+cont_conv <- function(x, theta = 0, nu = 5, eval = FALSE, ...) {
     if (is.numeric(x))
         return(as.data.frame(x))
-    if (!inherits(x, "data.frame"))
-        x <- as.data.frame(x)
-    as.data.frame(sapply(x, cont_conv_one, b = b, ell = ell))
+    x <- expand_as_numeric(x)
+    if (eval) cc_add_noise(x) else x
 }
 
-cont_conv_one <- function(x, b, ell) {
+#' Numeric model matrix for continuous convolution
+#'
+#' Turns ordered variables into integers and expands factors as binary dummy
+#' codes. [cc()] additionally adds noise to discrete variables, but this is only
+#' useful for estimation. `[cc_prepare()]` can be used to evaluate an already
+#' fitted estimate.
+#'
+#' @param x a vector or data frame with numeric, ordered, or factor columns.
+#'
+#' @return A numeric matrix containing the expanded variables. It has additional
+#'   type `expanded_as_numeric` and `attr(, "i_disc")` cntains the indices of
+#'   discrete variables.
+#'
+#' @noRd
+expand_as_numeric <- function(x) {
+    if (!inherits(x, "data.frame"))
+        x <- as.data.frame(x)
+    # which variables will be discrete in the output data frame?
+    i_disc <- get_i_disc(x)
+
+    # ordered -> integer, factors -> dummy coding
+    x <- do.call(cbind, lapply(x, cc_prepare_one))
+
+    # indicate which variables are discrete
+    attr(x, "i_disc") <- i_disc
+    class(x) <- c("matrix", "expanded_as_numeric")
+
+    x
+}
+
+cc_prepare_one <- function(x) {
     if (is.numeric(x)) {
-        return(x)
+        # nothing to do
     } else if (is.ordered(x)) {
-        return(as.numeric(x) + rupsb(length(x), b, ell))
+        x <- as.numeric(x)
     } else if (is.factor(x)) {
-        stop("x must be either numeric or ordered; \n",
-             "redefine the variables as ordered and/or ",
-             "call again with 'model.frame(~ x)' for dummy coding.")
+        # expand factors, first column is intercept
+        x <- model.matrix(~ x)[, -1, drop = FALSE]
+        colnames(x) <- paste0(seq.int(ncol(x)))
+    } else if (is.character(x)) {
+        stop("Don't know how to treat character variables; ",
+             "use either numeric, ordered, or factor.")
     } else {
-        stop("x has unsupported type ", class(x))
+        stop("x has unsupported type (", class(x), "); ",
+             "use either numeric, ordered, or factor.")
     }
+    x
+}
+
+get_i_disc <- function(x)
+    which(unlist(lapply(x, is_disc)))
+
+is_disc <- function(x) {
+    if (is.numeric(x)) {
+        return(FALSE)
+    } else if (is.ordered(x)) {
+        return(TRUE)
+    } else {
+        return(rep(TRUE, length(levels(x)) - 1))
+    }
+}
+
+#' Add noise to discrete variables
+#'
+#' @param x data matrix created by `expand_as_numeric()` (this is important,
+#'   because we need to know which variables are discrete).
+#' @noRd
+cc_add_noise <- function(x, theta = 0, nu = 5) {
+    stopifnot(inherits(x, "cc_prepared"))
+    i_disc <- attr(x, "i_disc")
+    n_disc <- length(i_disc)
+    if (n_disc > 1)
+        x[, i_disc] <- x[, i_disc] + rusb(n_disc * nrow(x), nrow(x), n_disc)
+
+    x
 }
 
--- tests/testthat/test-cont_conv.R
+++ tests/testthat/test-cont_conv.R
@@ -1,33 +1,29 @@
-context("Continuous convolution (cont_cov)")
+context("Continuous convolution")
 
 # dummy data
 dat <- data.frame(
+    F1 = factor(rbinom(10, 4, 0.1), 0:4),
     Z1 = as.ordered(rbinom(10, 5, 0.5)),
-    X1 = rnorm(10),
     Z2 = as.ordered(rpois(10, 1)),
+    X1 = rnorm(10),
     X2 = rexp(10)
 )
 
-test_that("does not add noise to continuous variables", {
-    dat_cc <- cont_conv(dat)
-    i_cnt <- which(sapply(dat, is.numeric))
-    sapply(i_cnt, function(i) expect_equal(dat[, i], dat_cc[ , i]))
+test_that("expands (only) factors", {
+    expect_equal(ncol(cont_conv(dat)), 8)
 })
 
-test_that("does add noise to ordered variables", {
+test_that("cont_cov adds noise to the right variables", {
     dat_cc <- cont_conv(dat)
-    i_cnt <- which(sapply(dat, is.ordered))
+    i_disc <- attr(dat_cc, "i_disc")
+    i_cnt <- setdiff(seq.int(ncol(dat)), i_disc)
+    sapply(i_cnt, function(i) expect_equal(dat[, i], dat_cc[ , i]))
     sapply(i_cnt, function(i) expect_false(all(dat[, i] == dat_cc[ , i])))
 })
 
-test_that("preserves input type", {
-    expect_equal(class(dat), class(cont_conv(dat)))
-    dat_num <- sapply(dat, as.numeric)
-    expect_equal(class(dat), class(cont_conv(dat)))
-})
-
-test_that("throws an error message for factors", {
-    dat_with_fct <- dat
-    dat_with_fct$F1 <- as.factor(1:10)
-    expect_error(cont_conv(dat_with_fct))
+test_that("throws error for characters or other", {
+    dat$char <- rep("A", 10)
+    expect_error(cont_conv(dat))
+    dat$char <- complex(1:10)
+    expect_error(cont_conv(dat))
 })
