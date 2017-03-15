8a9f3f965856e1c13d0215d814aecc16559d44e8
tnagler <thomas.nagler@tum.de>
2017-03-15 20:18:59 +0100

    expand usb tests

--- tests/testthat/test-usb.R
+++ tests/testthat/test-usb.R
@@ -10,9 +10,15 @@ test_that("Density integrates to one", {
 })
 
 test_that("Simulation works", {
-    expect_is(rusb(10), "numeric")
-    expect_is(rusb(10, theta = 0.4), "numeric")
-    expect_is(rusb(10, theta = 0.4, nu = 10), "numeric")
-    expect_is(rusb(10, theta = 0.4, nu = 0), "numeric")
+    expect_lt(max(abs(rusb(100))), 0.5)
+    expect_is(rusb(100), "numeric")
+    expect_lt(max(abs(rusb(100, theta = 0.4))), 0.9)
+    expect_is(rusb(100, theta = 0.4, nu = 10), "numeric")
 })
 
+test_that("qrng works", {
+    expect_lt(max(abs(rusb(100, quasi = TRUE))), 0.5)
+    expect_is(rusb(100, quasi = TRUE), "numeric")
+    expect_lt(max(abs(rusb(100, theta = 0.4, quasi = TRUE))), 0.9)
+    expect_is(rusb(100, theta = 0.4, nu = 10, quasi = TRUE), "numeric")
+})
